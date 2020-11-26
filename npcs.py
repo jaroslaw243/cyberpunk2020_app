a0dd9937153e84e16dc97d7a8dcd12358705c309
jaroslaw243 <boss.mielczarek@gmail.com>
2020-11-26 23:11:38 +0100

    npcs for campaign

--- npcs.py
+++ npcs.py
@@ -1,16 +1,33 @@
-valkyrie = {'dodgebase': 18, 'armorhead': 20, 'armortorso': 10, 'armorlefthand': 10, 'armorrighthand': 10,
-            'armorleftleg': 6, 'armorrightleg': 6, 'hard_armor': True, 'BC': 7, 'name': 'Valkyrie'}
-cultist_officer = {'dodgebase': 10, 'armorhead': 25, 'armortorso': 20, 'armorlefthand': 20, 'armorrighthand': 20,
-                   'armorleftleg': 20, 'armorrightleg': 20, 'hard_armor': True, 'BC': 10, 'name': 'Cultist officer'}
-cultist_elite = {'dodgebase': 12, 'armorhead': 20, 'armortorso': 18, 'armorlefthand': 18, 'armorrighthand': 18,
-                 'armorleftleg': 14, 'armorrightleg': 14, 'hard_armor': False, 'BC': 8, 'name': 'Elite cultist'}
-cultist_basic = {'dodgebase': 10, 'armorhead': 20, 'armortorso': 4, 'armorlefthand': 4, 'armorrighthand': 4,
-                 'armorleftleg': 4, 'armorrightleg': 4, 'hard_armor': False, 'BC': 5, 'name': 'Basic cultist'}
+# valkyrie = {'dodgebase': 18, 'armorhead': 20, 'armortorso': 10, 'armorlefthand': 10, 'armorrighthand': 10,
+#             'armorleftleg': 6, 'armorrightleg': 6, 'hard_armor': True, 'BC': 7, 'name': 'Valkyrie'}
+# cultist_officer = {'dodgebase': 10, 'armorhead': 25, 'armortorso': 20, 'armorlefthand': 20, 'armorrighthand': 20,
+#                    'armorleftleg': 20, 'armorrightleg': 20, 'hard_armor': True, 'BC': 10, 'name': 'Cultist officer'}
+# cultist_elite = {'dodgebase': 12, 'armorhead': 20, 'armortorso': 18, 'armorlefthand': 18, 'armorrighthand': 18,
+#                  'armorleftleg': 14, 'armorrightleg': 14, 'hard_armor': False, 'BC': 8, 'name': 'Elite cultist'}
+cop = {'dodgebase': 12, 'armorhead': 20, 'armortorso': 18, 'armorlefthand': 18, 'armorrighthand': 18,
+       'armorleftleg': 14, 'armorrightleg': 14, 'hard_armor': False, 'BC': 8, 'name': 'Cop'}
+# cultist_basic = {'dodgebase': 10, 'armorhead': 20, 'armortorso': 4, 'armorlefthand': 4, 'armorrighthand': 4,
+#                  'armorleftleg': 4, 'armorrightleg': 4, 'hard_armor': False, 'BC': 5, 'name': 'Basic cultist'}
+gangster = {'dodgebase': 10, 'armorhead': 20, 'armortorso': 4, 'armorlefthand': 4, 'armorrighthand': 4,
+            'armorleftleg': 4, 'armorrightleg': 4, 'hard_armor': False, 'BC': 5, 'name': 'Gangster'}
+gangster_guard = {'dodgebase': 4, 'armorhead': 14, 'armortorso': 14, 'armorlefthand': 14, 'armorrighthand': 20,
+                  'armorleftleg': 14, 'armorrightleg': 14, 'hard_armor': False, 'BC': 8, 'name': 'Gangster guard'}
 corp_grunt = {'dodgebase': 10, 'armorhead': 25, 'armortorso': 25, 'armorlefthand': 25, 'armorrighthand': 25,
-              'armorleftleg': 25, 'armorrightleg': 25, 'hard_armor': True, 'BC': 8, 'name': 'Corp grunt'}
-stockley_guard = {'dodgebase': 18, 'armorhead': 28, 'armortorso': 26, 'armorlefthand': 30, 'armorrighthand': 30,
-                  'armorleftleg': 10, 'armorrightleg': 10, 'hard_armor': True, 'BC': 9, 'name': 'Stockley guard'}
-militia = {'dodgebase': 8, 'armorhead': 20, 'armortorso': 4, 'armorlefthand': 4, 'armorrighthand': 4,
-           'armorleftleg': 4, 'armorrightleg': 4, 'hard_armor': False, 'BC': 5, 'name': 'Militia'}
+              'armorleftleg': 25, 'armorrightleg': 25, 'hard_armor': True, 'BC': 9, 'name': 'Corp grunt'}
+# stockley_guard = {'dodgebase': 18, 'armorhead': 28, 'armortorso': 26, 'armorlefthand': 30, 'armorrighthand': 30,
+#                   'armorleftleg': 10, 'armorrightleg': 10, 'hard_armor': True, 'BC': 9, 'name': 'Stockley guard'}
+corp_ninja = {'dodgebase': 18, 'armorhead': 28, 'armortorso': 26, 'armorlefthand': 30, 'armorrighthand': 30,
+              'armorleftleg': 10, 'armorrightleg': 10, 'hard_armor': True, 'BC': 9, 'name': 'Corp ninja'}
+# militia = {'dodgebase': 8, 'armorhead': 20, 'armortorso': 4, 'armorlefthand': 4, 'armorrighthand': 4,
+#            'armorleftleg': 4, 'armorrightleg': 4, 'hard_armor': False, 'BC': 5, 'name': 'Militia'}
+dwain = {'dodgebase': 6, 'armorhead': 18, 'armortorso': 48, 'armorlefthand': 36, 'armorrighthand': 36,
+         'armorleftleg': 24, 'armorrightleg': 24, 'hard_armor': False, 'BC': 8, 'name': 'Dwain'}
+fix = {'dodgebase': 7, 'armorhead': 6, 'armortorso': 14, 'armorlefthand': 14, 'armorrighthand': 14,
+       'armorleftleg': 34, 'armorrightleg': 34, 'hard_armor': False, 'BC': 4, 'name': 'Fixer'}
+mr_ironwood = {'dodgebase': 4, 'armorhead': 10, 'armortorso': 34, 'armorlefthand': 28, 'armorrighthand': 28,
+               'armorleftleg': 22, 'armorrightleg': 22, 'hard_armor': False, 'BC': 3, 'name': 'Mr Ironwood'}
+grace = {'dodgebase': 15, 'armorhead': 20, 'armortorso': 38, 'armorlefthand': 26, 'armorrighthand': 20,
+         'armorleftleg': 16, 'armorrightleg': 16, 'hard_armor': False, 'BC': 8, 'name': 'Grace Silver'}
 
-all_npcs = (valkyrie, cultist_officer, cultist_elite, cultist_basic, corp_grunt, stockley_guard, militia)
+
+all_npcs = (corp_grunt, corp_ninja, gangster, gangster_guard, cop, dwain, fix, mr_ironwood, grace)
